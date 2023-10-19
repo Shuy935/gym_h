d6f920818474b02692fbe2d215e563b3558c2c72
Iram Terrazas <iramgustavo235@gmail.com>
2023-10-19 09:16:02 -0600

    Login function enabled

--- lib/main.dart
+++ lib/main.dart
@@ -5,42 +5,41 @@ import 'package:gym_h/screens/auth_page.dart';
 import 'package:gym_h/screens/verify_email_page.dart';
 import 'package:gym_h/utils/utils.dart';
 
-Future <void> main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   runApp(MyApp());
 }
 
-final navigatorKey = GlobalKey <NavigatorState>();
+final navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   static final String title = 'Firebase Auth';
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-    scaffoldMessengerKey: Utils.messengerKey,
-    navigatorKey: navigatorKey,
-    debugShowCheckedModeBanner: false,
-    title: title,
-    theme: ThemeData.dark().copyWith(
-    ),
-    home: MainPage(),
-  );
+        scaffoldMessengerKey: Utils.messengerKey,
+        navigatorKey: navigatorKey,
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData.dark().copyWith(),
+        home: MainPage(),
+      );
 }
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return VerifyEmailPage();
-        }else {
-          return AuthPage();
-        }
-      },
-    ),
-  );
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return VerifyEmailPage();
+            } else {
+              return AuthPage();
+            }
+          },
+        ),
+      );
 }
