08b6a545e0345da6771deaf1c02b9241f0673168
Maria Isabel Chacon Rodriguez <isar509@gmail.com>
2023-11-08 17:19:06 -0600

    detallitos

--- lib/widget/interfaces/RegistrosH.dart
+++ lib/widget/interfaces/RegistrosH.dart
@@ -106,16 +106,13 @@ class _RegistroHState extends State<RegistroH> {
                                   2000), //DateTime.now() - not to allow to choose before today.
                               lastDate: DateTime(2101));
                           if (pickedDate != null) {
-                            print(
-                                pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+                            // print(pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
                             String formattedDate =
                                 DateFormat('yyyy-MM-dd').format(pickedDate);
-                            print(
-                                formattedDate); //formatted date output using intl package =>  2021-03-16
+                            // print(formattedDate); //formatted date output using intl package =>  2021-03-16
                             //you can implement different kind of Date Format here according to your requirement
 
                             setState(() {
-                              //TextEditingController dateinput =TextEditingController();
                               fechaController.text =
                                   formattedDate; //set output date to TextField value.
                             });
