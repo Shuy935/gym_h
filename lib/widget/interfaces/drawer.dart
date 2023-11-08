f8450848af8cf16e17b83b81172e3e408a90d811
Iram Terrazas <iramgustavo235@gmail.com>
2023-11-07 19:26:09 -0600

    Cambios en el drawer

--- lib/widget/interfaces/drawer.dart
+++ lib/widget/interfaces/drawer.dart
@@ -3,32 +3,63 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gym_h/models/users_model.dart';
 import 'package:gym_h/screens/user/profile.dart';
 
-class DrawerProfile extends StatelessWidget {
+class DrawerProfile extends StatefulWidget {
   const DrawerProfile({super.key});
+
+  @override
+  State<DrawerProfile> createState() => _DrawerProfile();
+}
+
+class _DrawerProfile extends State<DrawerProfile> {
+  String? username;
+  String? email;
+  @override
+  void initState() {
+    super.initState();
+    // Obtén el nombre del usuario antes de construir el cajón de navegación
+    getUserName();
+    getUserEmail();
+  }
+
+  Future<void> getUserName() async {
+    final userData = await readUser();
+    if (userData != null && userData.isNotEmpty) {
+      final user = userData[0]; // Suponemos que solo hay un usuario
+      setState(() {
+        username = user.fullname!;
+      });
+    }
+  }
+
+  Future<void> getUserEmail() async {
+    final userData = await readUser();
+    if (userData != null && userData.isNotEmpty) {
+      final user = userData[0]; // Suponemos que solo hay un usuario
+      setState(() {
+        email = user.email!;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          const DrawerHeader(
-            decoration: BoxDecoration(
+          UserAccountsDrawerHeader(
+            decoration: const BoxDecoration(
               color: Color.fromRGBO(215, 123, 255, 1),
             ),
-            child: Text(
-              'Home',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-              ),
-            ),
+            accountName: Text(username ?? 'User Fullname'),
+            accountEmail: Text(email ?? 'Email'),
           ),
           ListTile(
             leading: const Icon(Icons.person),
             title: const Text('Porfile'),
             onTap: () {
               final route =
-                  MaterialPageRoute(builder: (context) => TheProfile());
+                  MaterialPageRoute(builder: (context) => const TheProfile());
               Navigator.push(context, route);
             },
           ),
