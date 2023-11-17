5b533dcda83c509033f063b182eec02d1e4d14c6
Shuy935 <jessus_2500@hotmail.com>
2023-11-16 18:16:31 -0600

    Rutina terminada

--- lib/widget/interfaces/usuario/rutina.dart
+++ lib/widget/interfaces/usuario/rutina.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(Rutinas());
+}
+
 class Rutinas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -26,9 +30,10 @@ class Rutinas extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              CardR(
-                cantidad: cantidad,
-              ),
+              for (int index = 0; index < cantidad; index++)
+                CardR(
+                  cardIndex: index,
+                ),
             ],
           ),
         ),
@@ -38,175 +43,199 @@ class Rutinas extends StatelessWidget {
 }
 
 class CardR extends StatefulWidget {
-  final int cantidad;
+  final int cardIndex;
 
-  const CardR({Key? key, required this.cantidad}) : super(key: key);
+  const CardR({Key? key, required this.cardIndex}) : super(key: key);
 
   @override
   _CardRState createState() => _CardRState();
 }
 
 class _CardRState extends State<CardR> {
+  Color color = const Color.fromARGB(255, 72, 72, 72);
+
   @override
   Widget build(BuildContext context) {
-    print(widget.cantidad);
-    List<Widget> cards = [];
-    for (int index = 0; index < widget.cantidad; index++) {
-      Color color = const Color.fromARGB(255, 72, 72, 72);
-      bool cardVisibility = true;
-      cards.add(
-        Card(
-          color: const Color.fromARGB(255, 58, 58, 59),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
                   children: [
-                    Row(
-                      children: [
-                        Container(
-                          width: 120,
-                          child: const Text('Nombre: '),
-                        ),
-                        const Expanded(child: Text('get del ejercicio')),
-                      ],
-                    ),
                     Container(
-                      height: 10,
+                      width: 120,
+                      child: const Text('Nombre: '),
                     ),
-                    Row(
-                      children: [
-                        Container(
-                          width: 120,
-                          child: const Text('Repeticiones: '),
-                        ),
-                        const Expanded(
-                          child: Text('data'),
-                        ),
-                      ],
+                    const Expanded(child: Text('get del ejercicio')),
+                  ],
+                ),
+                Container(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 120,
+                      child: const Text('Repeticiones: '),
                     ),
-                    Row(
-                      children: [
-                        Container(
-                          width: 120,
-                          child: const Text('Series: '),
-                        ),
-                        const Expanded(
-                          child: Text('data'),
-                        ),
-                      ],
+                    const Expanded(
+                      child: Text('data'),
                     ),
                   ],
                 ),
                 Row(
                   children: [
                     Container(
-                      width: 150,
-                      height: 150,
-                      child: const Image(
-                        image: AssetImage('assets/image/logo.png'),
-                      ),
+                      width: 120,
+                      child: const Text('Series: '),
                     ),
+                    const Expanded(
+                      child: Text('data'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: const Image(
+                    image: AssetImage('assets/image/logo.png'),
+                  ),
+                ),
+                Container(
+                  width: 25,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Descanso recomendado:'),
                     Container(
-                      width: 25,
+                      width: 10,
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    const Text('Get de eso'),
+                    Container(
+                      height: 15,
+                    ),
+                    Row(
                       children: [
-                        const Text('Descanso recomendado:'),
+                        const Text('Dificultad:'),
                         Container(
                           width: 10,
                         ),
                         const Text('Get de eso'),
-                        Container(
-                          height: 15,
+                      ],
+                    ),
+                    Container(
+                      height: 25,
+                    ),
+                    Stack(
+                      children: <Widget>[
+                        const SizedBox(
+                          width: 150,
+                          height: 70,
                         ),
-                        Row(
-                          children: [
-                            const Text('Dificultad:'),
-                            Container(
-                              width: 10,
-                            ),
-                            const Text('Get de eso'),
-                          ],
+                        const Center(
+                          child: Text('Musculo'),
                         ),
-                        Container(
-                          height: 25,
-                        ),
-                        Stack(
-                          children: <Widget>[
-                            const SizedBox(
-                              width: 150,
-                              height: 70,
-                            ),
-                            const Center(
-                              child: Text('Musculo'),
-                            ),
-                            Positioned(
-                              left: 80,
-                              top: -5,
-                              child: ElevatedButton(
-                                style: ButtonStyle(backgroundColor:
-                                    MaterialStateProperty.resolveWith<Color?>(
-                                        (Set<MaterialState> states) {
-                                  // Cambiar el color en función del estado
+                        Positioned(
+                          left: 80,
+                          top: -5,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.resolveWith<Color?>(
+                                (Set<MaterialState> states) {
                                   if (states.contains(MaterialState.pressed)) {
-                                    // Estado cuando se presiona el botón por unos segundos
-                                    if (color == Color(0xff484848)) {
-                                      _mostrarCuadroConfirmacion(context, color);
-                                    }
-                                    return color;
+                                    Future.delayed(Duration.zero, () {});
                                   }
                                   return color;
-                                })),
-                                onPressed: () {
                                 },
-                                child: const Icon(Icons.check_circle_outline),
                               ),
-                            )
-                          ],
-                        ),
+                            ),
+                            onPressed: () {
+                              if (color == Color(0xff484848)) {
+                                _mostrarCuadroConfirmacion(context);
+                              } else {
+                                _mostrarCuadroCancelacion(context);
+                              }
+                            },
+                            child: const Icon(Icons.check_circle_outline),
+                          ),
+                        )
                       ],
                     ),
                   ],
-                )
+                ),
               ],
             ),
-          ),
+          ],
         ),
-      );
-    }
-    return Column(
-      children: cards,
+      ),
     );
   }
-}
 
-Future<void> _mostrarCuadroConfirmacion(BuildContext context,Color color) async {
-  return showDialog<void>(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Confirmación'),
-        content: Text('¿Ya realizo el ejercicio?'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('Cancelar'),
-          ),
-          TextButton(
-            onPressed: () {
-              //Futuro historial
-              Navigator.of(context).pop();
-            },
-            child: Text('Aceptar'),
-          ),
-        ],
-      );
-    },
-  );
+  Future<void> _mostrarCuadroConfirmacion(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirmación'),
+          content: Text('¿Ya realizó el ejercicio?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  color = Colors.green;
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text('Aceptar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _mostrarCuadroCancelacion(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Cancelacion'),
+          content: Text('¿Desea marcar el ejercicio seleccionado como no realizado?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  color = Color(0xff484848);
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text('Aceptar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
